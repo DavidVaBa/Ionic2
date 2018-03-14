6af917b54b15bdc6b7276983eeaa29b549d22684
David de Jhesus Vargas Banda <xoliu33@gmail.com>
2018-03-14 11:54:15 -0600

    Song mock, Song Service and Loading Provider

---
+++ src/mocks/songs/songs.mock.ts
@@ -0,0 +1,26 @@
+import { Song } from '../../models/song/song.interface';
+
+const songList: Song[] = [
+    {
+        "title":"Every Day",
+        "artist":"Eric Prydz",
+        "preview_url":"https://p.scdn.co/mp3-preview/42d3b15f67a8fc79b5f0d12636890f560ca2b540",
+        "image_small":"https://i.scdn.co/image/a93e836ad1fd9868639e00edc5a912d35f396d49",
+        "image_medium":"https://i.scdn.co/image/840e60d1f711f787c3336c9dbaffc2f86e17add7",
+        "image_large":"https://i.scdn.co/image/cec1837ea1dc6301fd558ce3db05b11ad36f1aaf",
+        "open_url":"https://open.spotify.com/track/63xYHNgkLuWrzoe6gNFWMl",
+        "song_id":"54dd0bc8a57fa90c00770675"
+    },
+    {
+        "title":"Don't Spill It",
+        "artist":"Mistah F.A.B.",
+        "preview_url":"https://p.scdn.co/mp3-preview/914e0823c3b201795c9e1ad0f4cf8fea970eba4a",
+        "image_small":"https://i.scdn.co/image/171b7edd15f88f380c100d97bbeb6e1c382f43ef",
+        "image_medium":"https://i.scdn.co/image/9167388ae969f8f33a6887be69b13487758df2b5",
+        "image_large":"https://i.scdn.co/image/c4c7a03a089e36050e0963022a11a36cab68724e",
+        "open_url":"https://open.spotify.com/track/7rYeFuCX9B0kvyrN0Qyl8z",
+        "song_id":"54dd0bc8a57fa90c00770628"
+    }
+]
+
+export const SONG_LIST = songList;
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { SongsService } from '../../providers/songs/songs';
+import { Song } from '../../models/song/song.interface';
+import { SONG_LIST } from '../../mocks/songs/songs.mock';
+import { LoadingProvider } from '../../providers/loading/loading';
 
+@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  songs: Song[];
 
+  constructor(public navCtrl: NavController, public navParams: NavParams, private SongsService: SongsService, private LoadingProvider: LoadingProvider) {
+    this.LoadingProvider.startLoad();
+
+    this.SongsService.getSongs().subscribe((data: Song[]) => {
+      this.songs = data;
+    })
+
+    this.LoadingProvider.endLoad();
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad HomePage');
   }
 
 }
